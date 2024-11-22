3a08aea0d348da1d3cc676fbd1c22613c4c2fe14
VeliySkyproAnton <bondarenkotanatos@mail.ru>
2024-11-22 18:35:57 +0300

    Update 2

--- src/main/java/com/stepintoprofession/bank/controller/ProductController.java
+++ src/main/java/com/stepintoprofession/bank/controller/ProductController.java
@@ -1,11 +1,19 @@
 package com.stepintoprofession.bank.controller;
 
+import com.stepintoprofession.bank.repository.ProductRepository;
 import com.stepintoprofession.bank.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<Object> viewProducts() {
+        return ResponseEntity.ok(productService.viewProducts());
+    }
 }
--- src/main/java/com/stepintoprofession/bank/service/ProductService.java
+++ src/main/java/com/stepintoprofession/bank/service/ProductService.java
@@ -1,11 +1,18 @@
 package com.stepintoprofession.bank.service;
 
+import com.stepintoprofession.bank.model.Product;
 import com.stepintoprofession.bank.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
+
+    public List<Product> viewProducts() {
+        return (List<Product>) productRepository.findAll();
+    }
 }
